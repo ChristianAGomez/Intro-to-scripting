029bb60200ea4d348d48fad1f90f90044d3bd8d5
Christian Gomez <christian.jatc@gmail.com>
2015-09-15 09:18:14 -0600

    Somthing

---
+++ Semester Project/Assets/Character.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+/*{
+	public class Stuff
+	{
+		public int bullets;
+		public int grenades;
+		public int rockets;
+
+		public Stuff(int bul, int gre, int roc)
+		{
+			bullets = bul;
+			grenades = gre;
+			rockets = roc;
+		}
+	}
+
+
+	public Stuff myStuff = new Stuff 10, 7, 25);
+	public float speed;
+	public float turnSpeed;
+	public Rigidbody bulletPrefab;
+	public Transform firePosition;
+	public float bulletSpeed;
+
+
+	void Update ()
+	{
+		Movement();
+		Shoot();
+	}
+
+
+	void Movement ()
+	{
+		float forwardMovement = Input.GetAxis("Vertical") * speed * Time.DeltaTime;
+		float turnMovement = Input.GetAxis ("Horizontal") * turnSpeed Time.DeltaTime;
+
+		transform.Translate(Vector3.forward * forwardMovement);
+		transform.Rotate(Vector3.up * turnMovement);
+	}
+
+
+	void Shoot ()
+	{ */
---
+++ Semester Project/Assets/Syntix.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntix : MonoBehaviour
+{
+	int myint = 5;
+
+
+	int MyFunction (int number)
+	{
+		int ret = myint * number;
+		return ret;
+	}
+}
