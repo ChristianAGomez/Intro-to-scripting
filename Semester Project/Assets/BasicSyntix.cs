27ce3479874159221e4ba87b350d84ff89171fe0
Christian Gomez <christian.jatc@gmail.com>
2015-09-15 09:38:27 -0600

    work

---
+++ Semester Project/Assets/BasicSyntix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicSyntix : MonoBehaviour
+{
+	void Start ()
+	{
+		Debug.Log (transform.position.x);
+
+
+		if(transform.position.y <= 5f)
+		{
+			Debug.Log ("HI!!!!!!!!!");
+		}
+	}
+}
