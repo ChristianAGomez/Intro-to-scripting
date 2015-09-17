8a40f32a4b65345198cf69aaff0e559bb6c7d913
Christian Gomez <christian.jatc@gmail.com>
2015-09-17 09:45:40 -0600

    Convention

--- Semester Project/Assets/conventionsandsyntaxpassoff.cs
+++ Semester Project/Assets/conventionsandsyntaxpassoff.cs
@@ -4,9 +4,7 @@ using System.Collections;
 public class conventionsandsyntaxpassoff : MonoBehaviour
 {
 	void Start ()
-	{
-       
-        }
+    { 
 		//This is a single line comment.
 		
 		/*This is a multiple line comment or block 
@@ -17,12 +15,12 @@ public class conventionsandsyntaxpassoff : MonoBehaviour
 	void Update()
 	{
 		//The following line of code is used to track the position on the falling cube. Enter the correct convention.
-		Debug.Log ();
+		Debug.Log (transform.position.x);
 
 		/*The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10. 
 		Enter the correct statement.*/
 
-		if () 
+		if (transform.position.y <= 10) 
 		{
 			Debug.Log ("I'm going to splater!");
 		}
