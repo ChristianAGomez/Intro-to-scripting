f5e712fe155ad14d449c28bf43a338c49b3195c7
Christian Gomez <christian.jatc@gmail.com>
2015-09-24 08:27:43 -0600

    If statement so far

---
+++ Semester Project/Assets/Ifstatment.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ifstatment : MonoBehaviour
+{
+    float HighJump = 85.0f;
+    float NormalLimitJump = 70.0f;
+    float LowLimitJump = 40.0f;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            JumpTest();
+
+        HighJump -= Time.deltaTime * 5f;
+        Debug.Log(HighJump);
+    }
+
+    void JumpTest()
+    {
+        if (HighJump > NormalLimitJump)
+        {
+            print("I can fly!");
+        }
+        else if (HighJump < LowLimitJump)
+        {
+            print("Who needs to jump?");
+        }
+        else
+        {
+            print("eh nothing special.");
+        }
+    }
+}
+
