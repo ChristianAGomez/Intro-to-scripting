8528e3c7d03fed2fbb734e723e3960444c278211
Christian Gomez <christian.jatc@gmail.com>
2015-09-29 09:07:52 -0600

    Loop coding complete

---
+++ Semester Project/Assets/DoWhile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoWhile : MonoBehaviour
+{
+    void Start ()
+    {
+        bool LetsStart = false;
+		
+        do
+        {
+            print("Goodbye school");
+
+		}while ( LetsStart== true);
+    }
+}
---
+++ Semester Project/Assets/Foreach.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foreach : MonoBehaviour
+{
+	void Start ()
+	{
+		string[] strings = new string[5];
+
+		strings[0] = "First string";
+		strings[3] = "Second string";
+		strings[4] = "Thrird string";
+
+		foreach(string item in strings)
+		{
+			print (item);
+		}
+	}
+}
