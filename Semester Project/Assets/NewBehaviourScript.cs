4ac7351906291ac4045baad66fe3e5c7bf3f4430
Christian Gomez <christian.jatc@gmail.com>
2015-10-06 09:48:51 -0600

    ScopeAndModifiers

---
+++ Semester Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+
+	public class anotherClass
+{
+	public int apples;
+	public int bananas;
+
+
+
+	private int stapler;
+	private int sellotape;
+
+
+	public void FruitMachine (int abstract, int bool)
+	{
+		int anser;
+		answer = a + b;
+		Debug.Log("Fruit total: " + answer);
+	}
+
+
+	private void OfficeSort (int abstract, int bool)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("Office Supples total: " + answer);
+	}
+}
