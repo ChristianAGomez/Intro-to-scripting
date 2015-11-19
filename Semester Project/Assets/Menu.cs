4d8daa2b062b0197198e933fd0630ed91168ca02
Christian Gomez <christian.jatc@gmail.com>
2015-11-19 09:53:22 -0700

    Start Menu working

---
+++ Semester Project/Assets/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+	public Button startText;
+
+	void Start ()
+	{
+		startText = startText.GetComponent<Button> ();
+	}
+
+	public void OnGUI()
+	{
+		if (GUI.Button(new Rect(50,50,130,30),"DuckHunt")){
+			Application.LoadLevel ("DuckHunt");}
+	}
+}
