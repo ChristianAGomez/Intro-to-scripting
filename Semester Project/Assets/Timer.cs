aa9b63852ba6991b09a576c0530d74833deabcbf
Christian Gomez <christian.jatc@gmail.com>
2015-12-01 09:50:52 -0700

    Might have a code for timer need to test and fix minor bugs

---
+++ Semester Project/Assets/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+	public Text counterText;
+
+	public float seconds, minutes;
+
+	void Start () {
+		counterText = GetComponent<Texture>() as Text;
+	}
+
+	void UpDate () {
+		minutes = (int)(Time.time/60f);
+		seconds = (int)(Time.time % 60f);
+		counterText.text = minutes.ToString ("00") + ":" + seconds.ToString("00");
+	}
+}
