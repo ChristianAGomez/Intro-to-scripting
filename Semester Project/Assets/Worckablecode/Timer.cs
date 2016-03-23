70afab3d885e365fbdb9e46ddfa4b91e9ab6f920
Christian Gomez <christian.jatc@gmail.com>
2016-03-23 09:54:35 -0600

    Scope access modifiers is the way to go. Need to rethink ScoreManager code

--- Semester Project/Assets/Worckablecode/Timer.cs
+++ Semester Project/Assets/Worckablecode/Timer.cs
@@ -5,13 +5,26 @@ using System.Collections;
 public class Timer : MonoBehaviour 
 {
 	public float timeRemaining = 30;
+    private ScoreManager myOtherClass;
+
+	private int FinalScore = 0;
+
+	void Start ()
+	{
+		FinalScore = 0;
+
+		myOtherClass = new ScoreManager ();
+		myOtherClass.score (FinalScore, myOtherClass.Score);
+	}
 
 	void Update ()
 	{
 		timeRemaining -= Time.deltaTime;
 		if(timeRemaining <=0)
+			if(Score <= 1&9)
 		{
-				Application.LoadLevel("GameOver");
+			Application.LoadLevel("Score 1-10");
+				//Application.LoadLevel("GameOver");
 		}
 	}
 }
